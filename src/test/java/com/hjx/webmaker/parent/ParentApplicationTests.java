6343645008ca2284766cde06add7f42217e1fefc
huangjixin <517714860@qq.com>
2021-07-15 15:57:48 +0800

    添加Kafka

--- src/test/java/com/hjx/webmaker/parent/ParentApplicationTests.java
+++ src/test/java/com/hjx/webmaker/parent/ParentApplicationTests.java
@@ -3,6 +3,9 @@ package com.hjx.webmaker.parent;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @SpringBootTest
 class ParentApplicationTests {
 
@@ -10,4 +13,10 @@ class ParentApplicationTests {
 	void contextLoads() {
 	}
 
+	public static void main(String[] args) {
+		Pattern p = Pattern.compile("^[0-9]*$");
+		Matcher m = p.matcher("APP9e3536");
+		boolean res = m.find();
+		System.out.println(res);
+	}
 }
